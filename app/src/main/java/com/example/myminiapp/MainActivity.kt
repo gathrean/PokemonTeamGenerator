d2ad21ea1efb6306476d7801aff7ab84278e6210
Gathrean Dela Cruz <gathrean@icloud.com>
2023-12-02 12:23:28 -0800

    Added my name and ID

--- app/src/main/java/com/example/myminiapp/MainActivity.kt
+++ app/src/main/java/com/example/myminiapp/MainActivity.kt
@@ -12,35 +12,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myminiapp.ui.theme.MyMiniAppTheme
 
+/**
+ * Name: Gathrean Dela Cruz
+ * Student ID: A01167248
+ */
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyMiniAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyMiniAppTheme {
-        Greeting("Android")
     }
 }
