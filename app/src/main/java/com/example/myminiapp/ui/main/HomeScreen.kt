d12ef326669301c32dba52c201ad431d542b95d4
Gathrean Dela Cruz <gathrean@icloud.com>
2023-12-03 11:15:39 -0800

    Made home screen text bigger Added icon to button Added Bulbasaur

--- app/src/main/java/com/example/myminiapp/ui/main/HomeScreen.kt
+++ app/src/main/java/com/example/myminiapp/ui/main/HomeScreen.kt
@@ -5,13 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 
 @Composable
 fun HomeScreen(onGenerateClick: () -> Unit) {
@@ -22,9 +28,17 @@ fun HomeScreen(onGenerateClick: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        AsyncImage(
+            model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
+
         Text(
+
             text = "Welcome to PokéTeam Generator!",
             color = Color.White,
+            fontSize = 24.sp,
             modifier = Modifier.padding(bottom = 16.dp)
         )
 
@@ -32,7 +46,15 @@ fun HomeScreen(onGenerateClick: () -> Unit) {
             onClick = { onGenerateClick() },
             modifier = Modifier.padding(bottom = 16.dp)
         ) {
-            Text(text = "Generate")
+            Text(
+                text = "Generate",
+                fontSize = 20.sp
+            )
+            Icon(
+                imageVector = Icons.Default.Refresh,
+                contentDescription = "Refresh",
+                modifier = Modifier.size(25.dp)
+            )
         }
     }
 }
