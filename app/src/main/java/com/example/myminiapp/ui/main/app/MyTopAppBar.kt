d7b496c99c003d4a94a89bf3219d11630848fd2a
Gathrean Dela Cruz <gathrean@icloud.com>
2023-12-03 16:12:03 -0800

    Reformat MyTopBar

--- app/src/main/java/com/example/myminiapp/ui/main/app/MyTopAppBar.kt
+++ app/src/main/java/com/example/myminiapp/ui/main/app/MyTopAppBar.kt
@@ -11,7 +11,11 @@ import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar(title: String, showDetails: Boolean, onBackClick: () -> Unit) {
+fun MyTopAppBar(
+    title: String,
+    showDetails: Boolean,
+    onBackClick: () -> Unit
+) {
     TopAppBar(
         title = { Text(text = if (showDetails) "Pokemon Details" else title) },
         navigationIcon = {
